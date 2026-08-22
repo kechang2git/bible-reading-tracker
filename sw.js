9ddffa01ccc825abb8ded5108b3cc59edb657b94
kechang2git <kechang2@gmail.com>
2026-08-21 23:13:07 -0700

    Bump PWA cache for finished records

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bible-reading-tracker-static-v4";
+const CACHE_NAME = "bible-reading-tracker-static-v5";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
